b54bb218f84a20068cc0ba5b54861cfe8006ec00
mayaekie071 <mayaekie071@SDS-L-191HC63>
2024-10-28 10:55:11 -0400

    Add project files.

---
+++ Adventure Game/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Adventure_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.option2Button = new System.Windows.Forms.Button();
+            this.option1Label = new System.Windows.Forms.Label();
+            this.option2Label = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.option1Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // option2Button
+            // 
+            this.option2Button.Location = new System.Drawing.Point(79, 324);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(38, 37);
+            this.option2Button.TabIndex = 0;
+            this.option2Button.UseVisualStyleBackColor = true;
+            // 
+            // option1Label
+            // 
+            this.option1Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.option1Label.ForeColor = System.Drawing.Color.White;
+            this.option1Label.Location = new System.Drawing.Point(123, 260);
+            this.option1Label.Name = "option1Label";
+            this.option1Label.Size = new System.Drawing.Size(340, 37);
+            this.option1Label.TabIndex = 3;
+            // 
+            // option2Label
+            // 
+            this.option2Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.option2Label.ForeColor = System.Drawing.Color.White;
+            this.option2Label.Location = new System.Drawing.Point(123, 324);
+            this.option2Label.Name = "option2Label";
+            this.option2Label.Size = new System.Drawing.Size(340, 37);
+            this.option2Label.TabIndex = 4;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(39, 9);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(431, 51);
+            this.outputLabel.TabIndex = 5;
+            // 
+            // option1Button
+            // 
+            this.option1Button.Location = new System.Drawing.Point(79, 260);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(38, 37);
+            this.option1Button.TabIndex = 6;
+            this.option1Button.UseVisualStyleBackColor = true;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(506, 409);
+            this.Controls.Add(this.option1Button);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.option2Label);
+            this.Controls.Add(this.option1Label);
+            this.Controls.Add(this.option2Button);
+            this.Name = "Form1";
+            this.Text = "Adventure Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.Label option1Label;
+        private System.Windows.Forms.Label option2Label;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button option1Button;
+    }
+}
+
---
+++ Adventure Game/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adventure_Game
+{
+    public partial class Form1 : Form
+    {
+        //Tracks what page the user is at
+        int page = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Display initial message and options
+
+            outputLabel.Text = "You wander upon a diner. Go inside?";
+            option1Label.Text = "Yes";
+            option2Label.Text = "No";
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 4;
+            }
+            else if (page == 3)
+            {
+                page = 1;
+            }
+            else if (page == 4)
+            {
+                page = 1;
+            }
+            else if (page == 5)
+            {
+                page = 6;
+            }
+            else if (page == 6)
+            {
+                page = 1;
+            }
+            else if (page == 7)
+            {
+                page = 1;
+            }
+            else if (page == 8)
+            {
+                page = 1;
+            }
+
+
+            /// Display text and game options to screen based on the 
+            /// current page
+
+            switch (page)
+            {
+                case 1:
+                    outputLabel.Text = "You're lost in a forest";
+                    option1Label.Text = "Go North";
+                    option2Label.Text = "Go South";
+
+                    break;
+                case 2:
+                    outputLabel.Text = "You find a lake. Take a drink?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+
+                    break;
+                case 3:
+                    outputLabel.Text = "You fall in a pit and die. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 4:
+                    outputLabel.Text = "The water is foul. You die of cholera. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 5:
+                    outputLabel.Text = "You find a horse. Do you ride it?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 6:
+                    outputLabel.Text = "You tame the horse and ride to safety. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 7:
+                    outputLabel.Text = "The horse walks past and you die of loneliness. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 8:
+                    outputLabel.Text = "You jump over a chasm to freedom. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 99:
+                    outputLabel.Text = "Thanks for playing !!";
+                    option1Label.Text = "";
+                    option2Label.Text = "";
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    }
+    
+}
