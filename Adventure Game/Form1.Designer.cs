c30510c656d80cb956e5eb171aa6e2bce8b6a110
mayaekie071 <mayaekie071@SDS-L-191HC63>
2024-11-06 11:01:42 -0500

    Nov6

--- Adventure Game/Form1.Designer.cs
+++ Adventure Game/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.option2Button = new System.Windows.Forms.Button();
             this.option1Label = new System.Windows.Forms.Label();
             this.option2Label = new System.Windows.Forms.Label();
@@ -37,16 +38,20 @@
             // 
             // option2Button
             // 
+            this.option2Button.BackColor = System.Drawing.Color.White;
             this.option2Button.Location = new System.Drawing.Point(79, 324);
             this.option2Button.Name = "option2Button";
             this.option2Button.Size = new System.Drawing.Size(38, 37);
             this.option2Button.TabIndex = 0;
-            this.option2Button.UseVisualStyleBackColor = true;
+            this.option2Button.UseVisualStyleBackColor = false;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
             // 
             // option1Label
             // 
-            this.option1Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.option1Label.ForeColor = System.Drawing.Color.White;
+            this.option1Label.BackColor = System.Drawing.Color.White;
+            this.option1Label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.option1Label.Font = new System.Drawing.Font("Courier New", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1Label.ForeColor = System.Drawing.Color.LightPink;
             this.option1Label.Location = new System.Drawing.Point(123, 260);
             this.option1Label.Name = "option1Label";
             this.option1Label.Size = new System.Drawing.Size(340, 37);
@@ -54,8 +59,10 @@
             // 
             // option2Label
             // 
-            this.option2Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.option2Label.ForeColor = System.Drawing.Color.White;
+            this.option2Label.BackColor = System.Drawing.Color.White;
+            this.option2Label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.option2Label.Font = new System.Drawing.Font("Courier New", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2Label.ForeColor = System.Drawing.Color.LightPink;
             this.option2Label.Location = new System.Drawing.Point(123, 324);
             this.option2Label.Name = "option2Label";
             this.option2Label.Size = new System.Drawing.Size(340, 37);
@@ -63,32 +70,37 @@
             // 
             // outputLabel
             // 
-            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.BackColor = System.Drawing.Color.White;
+            this.outputLabel.Font = new System.Drawing.Font("Courier New", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.LightPink;
             this.outputLabel.Location = new System.Drawing.Point(39, 9);
             this.outputLabel.Name = "outputLabel";
-            this.outputLabel.Size = new System.Drawing.Size(431, 51);
+            this.outputLabel.Size = new System.Drawing.Size(431, 108);
             this.outputLabel.TabIndex = 5;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // option1Button
             // 
+            this.option1Button.BackColor = System.Drawing.Color.White;
             this.option1Button.Location = new System.Drawing.Point(79, 260);
             this.option1Button.Name = "option1Button";
             this.option1Button.Size = new System.Drawing.Size(38, 37);
             this.option1Button.TabIndex = 6;
-            this.option1Button.UseVisualStyleBackColor = true;
+            this.option1Button.UseVisualStyleBackColor = false;
             this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
+            this.BackColor = System.Drawing.Color.Linen;
             this.ClientSize = new System.Drawing.Size(506, 409);
             this.Controls.Add(this.option1Button);
             this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.option2Label);
             this.Controls.Add(this.option1Label);
             this.Controls.Add(this.option2Button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Adventure Game";
             this.ResumeLayout(false);
--- Adventure Game/Form1.cs
+++ Adventure Game/Form1.cs
@@ -34,25 +34,23 @@ namespace Adventure_Game
             }
             else if (page == 2)
             {
-                page = 4;
-            }
-            else if (page == 3)
-            {
-                page = 1;
+                Random randGen = new Random();
+                int randomValue = randGen.Next(1, 101);
+
+                if (randomValue > 70)
+                {
+                    page = 4;
+                }
+                else
+                {
+                    page = 5;
+                }
             }
             else if (page == 4)
             {
                 page = 1;
             }
-            else if (page == 5)
-            {
-                page = 6;
-            }
             else if (page == 6)
-            {
-                page = 1;
-            }
-            else if (page == 7)
             {
                 page = 1;
             }
@@ -60,7 +58,22 @@ namespace Adventure_Game
             {
                 page = 1;
             }
-
+            else if (page == 10)
+            {
+                page = 12;
+            }
+            else if (page == 12)
+            {
+                page = 1;
+            }
+            else if (page == 14)
+            {
+                page = 1;
+            }
+            else if (page == 16)
+            {
+                page = 1;
+            }
 
             /// Display text and game options to screen based on the 
             /// current page
@@ -68,44 +81,252 @@ namespace Adventure_Game
             switch (page)
             {
                 case 1:
-                    outputLabel.Text = "You're lost in a forest";
-                    option1Label.Text = "Go North";
-                    option2Label.Text = "Go South";
-
-                    break;
-                case 2:
-                    outputLabel.Text = "You find a lake. Take a drink?";
+                    outputLabel.Text = "You wander upon a diner. Go inside?";
                     option1Label.Text = "Yes";
                     option2Label.Text = "No";
 
                     break;
+                case 2:
+                    outputLabel.Text = "An old woman greets you and brings you a menu. What do you order?";
+                    option1Label.Text = "Milkshake";
+                    option2Label.Text = "Coffee";
+
+                    break;
                 case 3:
-                    outputLabel.Text = "You fall in a pit and die. Play again?";
+                    outputLabel.Text = "You turn around and get hit by a car. Play again?";
                     option1Label.Text = "Yes";
                     option2Label.Text = "No";
                     break;
                 case 4:
-                    outputLabel.Text = "The water is foul. You die of cholera. Play again?";
+                    outputLabel.Text = "It was poisoned and you die. Play again?";
                     option1Label.Text = "Yes";
                     option2Label.Text = "No";
                     break;
                 case 5:
-                    outputLabel.Text = "You find a horse. Do you ride it?";
-                    option1Label.Text = "Yes";
-                    option2Label.Text = "No";
+                    outputLabel.Text = "You enjoy your drink and ask for the bill. How much do you tip?";
+                    option1Label.Text = "Nothing";
+                    option2Label.Text = "2.00$";
                     break;
                 case 6:
-                    outputLabel.Text = "You tame the horse and ride to safety. Play again?";
+                    outputLabel.Text = "The old lady gets upset and kills you. Play again?";
                     option1Label.Text = "Yes";
                     option2Label.Text = "No";
                     break;
                 case 7:
-                    outputLabel.Text = "The horse walks past and you die of loneliness. Play again?";
+                    outputLabel.Text = "You pay and leave. It's getting dark out, " +
+                        "do you call a taxi or walk home?";
+                    option1Label.Text = "Call a Taxi";
+                    option2Label.Text = "Walk Home";
+                    break;
+                case 8:
+                    outputLabel.Text = "You get lost and eaten by wolves. Play again?";
                     option1Label.Text = "Yes";
                     option2Label.Text = "No";
                     break;
+                case 9:
+                    outputLabel.Text = "You call a taxi and wait for it to arrive." +
+                        "While you're driving, you notice this isn't the way to your house." +
+                        "Do you mention this?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 10:
+                    outputLabel.Text = "Your driver apologizes for the mistake and turns around." +
+                        "Once you're home you get ready for bed. You hear a noise. Do you ignore it?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 11:
+                    outputLabel.Text = "You get kidnapped and die. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 12:
+                    outputLabel.Text = "Trying to find the noise, you trip down the stairs and break your neck." +
+                        "Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 13:
+                    outputLabel.Text = "You ignore it and fall asleep. The next day you wakeup and get ready " +
+                        "for work. As you're leaving you grab a snack. What do you choose?";
+                    option1Label.Text = "Apple";
+                    option2Label.Text = "Muffin";
+                    break;
+                case 14:
+                    outputLabel.Text = "You choke and die. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 15:
+                    outputLabel.Text = "You enjoy the muffin on your drive. While approaching a traffic light" +
+                        "it turns yellow. Do you speed up or slow down?";
+                    option1Label.Text = "Speed thru";
+                    option2Label.Text = "Slow down";
+                    break;
+                case 16:
+                    outputLabel.Text = "You call a atxi and wait for it to arrive." +
+                        "While you're driving, you notice this isn't the way to your house." +
+                        "Do you mention this?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 17:
+                    outputLabel.Text = "You call a taxi and wait for it to arrive." +
+                        "While you're driving, you notice this isn't the way to your house." +
+                        "Do you mention this?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 99:
+                    outputLabel.Text = "Thanks for playing !!";
+                    option1Label.Text = "";
+                    option2Label.Text = "";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 3)
+            {
+                page = 99;
+            }
+            else if (page == 5)
+            {
+                page = 7;
+            }
+            else if (page == 7)
+            {
+                page = 9;
+            }
+            else if (page == 9)
+            {
+                page = 11;
+            }
+            else if (page == 11)
+            {
+                page = 99;
+            }
+            else if (page == 13)
+            {
+                page = 15;
+            }
+            else if (page == 15)
+            {
+                page = 17;
+            }
+            else if (page == 17)
+            {
+                page = 99;
+            }
+
+            /// Display text and game options to screen based on the 
+            /// current page
+
+            switch (page)
+            {
+                case 1:
+                    outputLabel.Text = "You wander upon a diner. Go inside?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+
+                    break;
+                case 2:
+                    outputLabel.Text = "An old woman greets you and brings you a menu. What do you order?";
+                    option1Label.Text = "Milkshake";
+                    option2Label.Text = "Coffee";
+
+                    break;
+                case 3:
+                    outputLabel.Text = "You turn around and get hit by a car. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 4:
+                    outputLabel.Text = "It was poisoned and you die. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 5:
+                    outputLabel.Text = "You enjoy your drink and ask for the bill. How much do you tip?";
+                    option1Label.Text = "Nothing";
+                    option2Label.Text = "2.00$";
+                    break;
+                case 6:
+                    outputLabel.Text = "The old lady gets upset and kills you. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 7:
+                    outputLabel.Text = "You pay and leave. It's getting dark out, " +
+                        "do you call a taxi or walk home?";
+                    option1Label.Text = "Call a Taxi";
+                    option2Label.Text = "Walk Home";
+                    break;
                 case 8:
-                    outputLabel.Text = "You jump over a chasm to freedom. Play again?";
+                    outputLabel.Text = "You get lost and eaten by wolves. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 9:
+                    outputLabel.Text = "You call a taxi and wait for it to arrive." +
+                        "While you're driving, you notice this isn't the way to your house." +
+                        "Do you mention this?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 10:
+                    outputLabel.Text = "Your driver apologizes for the mistake and turns around." +
+                        "Once you're home you get ready for bed. You hear a noise. Do you ignore it?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 11:
+                    outputLabel.Text = "You get kidnapped and die. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 12:
+                    outputLabel.Text = "Trying to find the noise, you trip down the stairs and break your neck." +
+                        "Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 13:
+                    outputLabel.Text = "You ignore it and fall asleep. The next day you wakeup and get ready " +
+                        "for work. As you're leaving you grab a snack. What do you choose?";
+                    option1Label.Text = "Apple";
+                    option2Label.Text = "Muffin";
+                    break;
+                case 14:
+                    outputLabel.Text = "You choke and die. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 15:
+                    outputLabel.Text = "You enjoy the muffin on your drive. While approaching a traffic light" +
+                        "it turns yellow. Do you speed up or slow down?";
+                    option1Label.Text = "Speed thru";
+                    option2Label.Text = "Slow down";
+                    break;
+                case 16:
+                    outputLabel.Text = "You call a atxi and wait for it to arrive." +
+                        "While you're driving, you notice this isn't the way to your house." +
+                        "Do you mention this?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 17:
+                    outputLabel.Text = "You call a taxi and wait for it to arrive." +
+                        "While you're driving, you notice this isn't the way to your house." +
+                        "Do you mention this?";
                     option1Label.Text = "Yes";
                     option2Label.Text = "No";
                     break;
@@ -119,6 +340,7 @@ namespace Adventure_Game
             }
         }
     }
-    }
-    
 }
+    
+    
+
